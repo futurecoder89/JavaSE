65f5874975490b12363e94233bd626899420539b
futurecoder89 <blee.kcis@gmail.com>
2022-11-16 21:49:24 +0900

    Day 3일차 노트 입니다

---
+++ src/day03/operator/MainClass02.java
@@ -0,0 +1,29 @@
+package day03.operator;
+/*
+ * 2. 증감 연산자
+ * 
+ * 	++ : 1씩 증가 시킨다.
+ * 	-- : 1씩 감소 시킨다.
+ * 
+ * 
+ */
+public class MainClass02 {
+	public static void main(String[] args) {
+		int num = 0;
+		num = num +1;
+		System.out.println(num)
+		num++;
+		System.out.println(num);
+		
+		//0~99 까지 출력하는 반복문(for)
+		for(int i=0; i < 100; 1++) {
+			System.out.println(i);
+		}
+		
+		inu num2 = 0;
+		num2 = num2 - 1;
+		System.out.println(num2);
+		
+		
+	}
+}
